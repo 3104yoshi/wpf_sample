398edd67da55fc15ecb7b28e3aca4d924343210f
3104yoshi <3104adgjmp@gmail.com>
2023-02-28 22:04:03 +0900

    Add TriggerAction, close window

---
+++ Validation_Sample/Validation_Sample/TriggerAction/WindowCloseAction.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Validation_Sample.TriggerAction
+{
+    internal class WindowCloseAction : TriggerAction<Button>
+    {
+        protected override void Invoke(object parameter)
+        {
+            var window = Window.GetWindow((Button)AssociatedObject);
+            window.Close();
+        }
+    }
+}
